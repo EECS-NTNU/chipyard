daea75fd88e9c634b7a03e4e39cf62bc83b858f1
Amund Bergland Kvalsvik <amund.kvalsvik@ntnu.no>
2024-09-24 11:47:25 +0200

    added more boom configs

--- generators/chipyard/src/main/scala/config/BoomConfigs.scala
+++ generators/chipyard/src/main/scala/config/BoomConfigs.scala
@@ -20,13 +20,35 @@ class MediumBoomConfigRenTaint extends Config(
   new chipyard.config.AbstractConfig)
 
 class MediumBoomConfigRegTaint extends Config(
-  new boom.common.WithInOrderBranchResolution ++
   new boom.common.WithRegisterTaintTrackingEnabled ++
   new boom.common.WithNMediumBooms(1) ++
   new chipyard.config.AbstractConfig)
 
 class MediumBoomConfigNDA extends Config(
   new boom.common.WithNDAEnabled ++ 
+  new boom.common.WithNMediumBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MediumBoomConfigStats extends Config(
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMediumBooms(1) ++                         // medium boom config
+  new chipyard.config.AbstractConfig)
+
+class MediumBoomConfigNDAStats extends Config(
+  new boom.common.WithNDAEnabled ++ 
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMediumBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MediumBoomConfigRenTaintStats extends Config(
+  new boom.common.WithRenameTaintTrackingEnabled ++
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMediumBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MediumBoomConfigRegTaintStats extends Config(
+  new boom.common.WithRegisterTaintTrackingEnabled ++
+  new boom.common.WithTraceStats ++
   new boom.common.WithNMediumBooms(1) ++
   new chipyard.config.AbstractConfig)
 
@@ -41,7 +63,30 @@ class MegaBoomConfigRenTaint extends Config(
   new chipyard.config.AbstractConfig)
 
 class MegaBoomConfigNDA extends Config(
-  new boom.common.WithNDAEnabled ++ 
+  new boom.common.WithNDAEnabled ++
+  new boom.common.WithNMegaBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MegaBoomConfigStats extends Config(
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMegaBooms(1) ++                           // mega boom config
+  new chipyard.config.AbstractConfig)
+
+class MegaBoomConfigRegTaintStats extends Config(
+  new boom.common.WithRegisterTaintTrackingEnabled ++ 
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMegaBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MegaBoomConfigRenTaintStats extends Config(
+  new boom.common.WithRenameTaintTrackingEnabled ++ 
+  new boom.common.WithTraceStats ++
+  new boom.common.WithNMegaBooms(1) ++
+  new chipyard.config.AbstractConfig)
+
+class MegaBoomConfigNDAStats extends Config(
+  new boom.common.WithNDAEnabled ++  
+  new boom.common.WithTraceStats ++
   new boom.common.WithNMegaBooms(1) ++
   new chipyard.config.AbstractConfig)
 
